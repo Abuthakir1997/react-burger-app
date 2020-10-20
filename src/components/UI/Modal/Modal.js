4052c7008b3cb40a87b052ce832f5273641ae8b4
Abu thakir <abudx.at@gmail.com>
2020-10-20 16:16:52 +0530

    push

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ import classes from './Modal.module.css';
 const modal = (props) => (
     <Auxx>
         <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
-        <div className={classes.Modal} style={{ transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0', display: props.show ? 'block' : 'none', transition: props.show ? 'transform 1s ease-out' : 'none' }}>
+        <div className={classes.Modal} style={{ transform: props.show ? "translateY(0)" : "translateY(-100vh)", opacity: props.show ? '1' : '0' }}>
             {props.children}
         </div>
     </Auxx>
